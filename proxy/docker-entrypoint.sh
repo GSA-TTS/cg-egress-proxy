7da7579d3eddde9eb23234b80024c71e51a3b45c
Ryan Ahearn <ryan.ahearn@gsa.gov>
2025-08-29 10:58:48 -0400

    Replace .profile use with run.sh

--- proxy/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This entrypoint will facilitate a local Docker run of cg-egress-proxy coming as close as possible
-# to what happens on cloud.gov with the binary_buildpack
-
-# source .profile to set up allow.acl and deny.acl
-. .profile
-
-exec "$@"
